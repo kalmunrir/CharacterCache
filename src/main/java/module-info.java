1d6dbe37280d9d0947157c7a9f1544e8b0aeb6db
kalmunrir <jdoa2003@protonmail.com>
2024-07-23 19:16:10 -0600

    update dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.kalmunrir.charactercache {
     requires javafx.controls;
     requires javafx.fxml;
-
-
+    requires org.xerial.sqlitejdbc;
+    requires java.sql;
+    requires unirest.java;
+    requires org.json;
     opens com.kalmunrir.charactercache to javafx.fxml;
     exports com.kalmunrir.charactercache;
+    exports com.kalmunrir.charactercache.Database.Models;
 }
