9de398e2fe2e7075e07b3efbe5a5551636de2fb8
kalmunrir <jdoa2003@protonmail.com>
2024-07-24 22:19:59 -0600

    fill in create function

--- src/main/java/com/kalmunrir/charactercache/Database/DAOs/CRUDHelper.java
+++ src/main/java/com/kalmunrir/charactercache/Database/DAOs/CRUDHelper.java
@@ -1,15 +1,51 @@
 package com.kalmunrir.charactercache.Database.DAOs;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import org.sqlite.SQLiteException;
+
+import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class CRUDHelper {
-    public static void create(String tablename) {
-        
+    public static ResultSet create(String tablename, String[] columnNames, Object[] values, int[] types) {
+        StringBuilder sql = new StringBuilder("INSERT INTO " + tablename + " (");
+
+        // Building the query
+        for (int i = 0; i < columnNames.length; i++) {
+            sql.append(columnNames[i]);
+            if (i != columnNames.length - 1) {
+                sql.append(", ");
+            }
+        }
+        sql.append(") VALUES (");
+        for (int i = 0; i < values.length; i++) {
+            sql.append("?");
+            if (i != values.length - 1) {
+                sql.append(", ");
+            }
+        }
+        sql.append(");");
+
+        try (Connection conn = Database.connect()) {
+            PreparedStatement ps = conn.prepareStatement(sql.toString());
+            for (int i = 0; i < values.length; i++) {
+                if (types[i] == Types.INTEGER) {
+                    ps.setInt(i, (int) values[i]);
+                }
+                else if (types[i] == Types.VARCHAR) {
+                    ps.setString(i, (String) values[i]);
+                }
+            }
+            return ps.executeQuery();
+        } catch (SQLException e) {
+            Logger.getAnonymousLogger().log(
+                    Level.SEVERE,
+                    LocalDateTime.now() + ": Could not insert entry into " +
+                            tablename + " because " + e.getCause()
+            );
+            return null;
+        }
     }
 
     public static void read(String tablename) {
